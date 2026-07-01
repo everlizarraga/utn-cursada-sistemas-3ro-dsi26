66d86244dee706c8140b37b575a9914407782384
Ever Lizarraga <rolank.utn@gmail.com>
2026-07-01 18:19:56 -0300

    Estructura base - Primer modulo creado

---
+++ clase-02-practica-apis-jsonplaceholder/src/main/java/io/github/everlizarraga/practicaapisjsonplaceholder/Clase02PracticaApisJsonplaceholderApplication.java
@@ -0,0 +1,14 @@
+package io.github.everlizarraga.practicaapisjsonplaceholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase02PracticaApisJsonplaceholderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Clase02PracticaApisJsonplaceholderApplication.class, args);
+    System.out.println("Modulo 2 andando. !!!!!");
+  }
+
+}
